1e6a7cd9095cae5627f7cb39d368de7ff0a51435
adansyah125 <adansyah225@gmail.com>
2026-05-14 19:32:57 +0700

    fix:end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 import About from "./components/About";
+import Home from "./components/Home";
 import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
 import Outlet from "./components/Outlet";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
@@ -11,12 +11,12 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <About />
-      <Menu />
-      <Outlet />
-      <Gallery />
-      <Footer />
+     <Home />
+     <About />
+     <Menu />
+     <Outlet />
+     <Gallery />
+     <Footer />
     </>
   );
 }
